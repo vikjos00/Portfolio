c310a7d072fa3eaa3f98de19a98e7eed28a2e7c8
vikjos00 <82800751+vikjos00@users.noreply.github.com>
2024-03-14 16:22:19 +0100

    Javascript.js

--- Javascript/Javascript.js
+++ Javascript/Javascript.js
@@ -52,3 +52,63 @@ document.addEventListener("click", function (e) {
 function alert_message(){
   alert("This project is still being coded, try another one!");
 }
+
+
+function test2(){
+  setTimeout(typeWriter, 500);
+}
+
+var i = 0; // Index för nuvarande position i strängen
+var speed = 50; // Hastighet för att visa tecken
+
+// Funktion för att skriva text i ett element med typskrivareffekt
+function typeWriter(txt, elementId, callback) {
+  if (i < txt.length) {
+    document.getElementById(elementId).innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(function() { typeWriter(txt, elementId, callback); }, speed);
+  } else if (callback) {
+    i = 0; // Återställ index för nästa anrop
+    setTimeout(callback, 500); // Kalla på nästa funktion efter en fördröjning om det finns en callback
+  }
+}
+
+// Använd funktionen för att skriva ut txt_1 följt av txt_2
+function startTyping() {
+  var txt_1 = '<div class="about">';
+  var txt_2 = "</div>";
+  
+  typeWriter(txt_1, "text_load_1", function() {
+    typeWriter(txt_2, "text_load_2");
+  });
+  
+}
+setTimeout(content_load, 1200);
+setTimeout(startTyping2, 2000);
+setTimeout(content_load2, 3250);
+
+function startTyping2() {
+  var txt_3 = '      <ul class="skills">'
+  var txt_4 = "</ul>"
+  
+  typeWriter(txt_3, "text_load_3", function() {
+    typeWriter(txt_4, "text_load_4");
+  });
+  setTimeout(content_load2, 1200);
+}
+
+function content_load(){
+  var text_1 = document.getElementById("text_1");
+  var text_2 = document.getElementById("text_2");
+  
+  text_1.style.opacity = "1";
+  text_2.style.opacity = "1";
+}
+
+function content_load2(){
+  var text_3 = document.getElementById("text_3");
+  var text_4 = document.getElementById("text_4");
+  
+  text_3.style.opacity = "1";
+  text_4.style.opacity = "1";
+}
